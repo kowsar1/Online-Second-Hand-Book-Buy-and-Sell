a138f6101a05fcac1702117c212b539dfae21b5a
kowsar1 <kowsarsorkar112@gmail.com>
2022-12-22 15:05:41 +0600

    add about us

--- resources/views/backend/page/about.blade.php
+++ resources/views/backend/page/about.blade.php
@@ -5,34 +5,26 @@
 
 @section('content')
 
-
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+<section id="about" class="pt-10 mb-10">
+    <div class="container mx-auto px-5">
+        <div class="grid lg:grid-cols-2 md:grid-cols-2 overflow-hidden">
+            <div class="lg:w-2/1 md:w-2/1">
+                <h2 class="font-semibold mb-8 md:flex  text-3xl text-center text-deep-sapphire-600 ">AboutUs</h2>
+                <p class="leading-relaxed lg:text-lg md:text-lg text-sm tracking-wider text-gray-600">It’s a
+                    changing world with changing time and generation, so we assure to go accordingly.
+                    Our software company is mainly based on to value your thoughts and bring your requirements to a
+                    true form and also we intend with our learning platform ensuring that you have the right
+                    knowledge to show up your true worth, because you deserve to be valued.
+                    Interacting with you will please us to ensure your prime time being valued efficiently. We will
+                    honestly be assured on being supportive to build up your knowledge or your dream project.
+                </p>
+            </div>
+            <div>
+                <img class=" mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
+                                    
+                    src="https://www.kodeeo.com/images/gallery/about-us.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
